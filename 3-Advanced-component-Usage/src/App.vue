9d201a6eb756408962b8d68c03f74972b3a0464a
darshan.p <darshan.p@BBMDVDARSHANP.local>
2019-01-25 16:00:43 +0530

    section 9 slots, dynamic component and lifecycle hooks for dynamic components

---
+++ 3-Advanced-component-Usage/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+               <app-quote>
+                   <h1 slot="title">A Quote</h1>
+                   <p slot="content">A wonderful quote</p>
+               </app-quote>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Quote from './Components/Quote.vue'
+    export default {
+        components: {
+            appQuote: Quote
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ 3-Advanced-component-Usage/src/components/Quote.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div class="title">
+            <slot name="title"></slot>
+        </div>
+        <hr>
+        <div>
+            <slot name="content"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid #ccc;
+        box-shadow: 1px 1px 2px #000;
+        padding: 30px;
+        text-align: center;
+    }
+    h1 {
+        color: red;
+    }
+</style>
